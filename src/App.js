f7e1ef6e602230ad48ade70021dc9e7a51ec4ed0
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2023-10-21 17:16:20 -0300

    iniciando estrutura inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
 
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+} from "react-router-dom"
+
+import Users from "./pages/Users";
+import Home from "./pages/Home";
+
+
+
+// componente react = funcao js que retorna JSX
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( // JSX dentro do return
+        <Router>
+            <Routes>
+                <Route path="/users" element={<Users />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
+    )
 }
 
-export default App;
+export default App // syntax sugar
---
+++ src/pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react"
+import { Link } from 'react-router-dom'
+
+
+function Home() {
+    return (
+        <>
+            <h1>Pagina Inicial</h1>
+            <ul>
+                <li>
+                    <Link to="/users">Usuários</Link>
+                </li>
+            </ul>
+        </>
+    )
+}
+
+export default Home
