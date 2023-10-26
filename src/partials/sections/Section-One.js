ef9604b66d93ef7210ce1f2702f57487c4622147
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2023-10-25 21:14:30 -0300

    adicionando cards

--- src/partials/sections/Section-One.js
+++ src/partials/sections/Section-One.js
@@ -4,15 +4,19 @@ import { styled } from "@mui/material"
 
 const StyledSection = styled('section')(() => ({
     height: '100vh',
-
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    paddingBottom: '100px',
 }))
 
 const StyledBackground = styled('div')(() => ({
     backgroundImage: 'url(https://static.wixstatic.com/media/b65cfd_12fbe66f7a584cb3bf2d56f03fcf8378~mv2.jpg/v1/fill/w_988,h_684,al_c,q_85,enc_auto/b65cfd_12fbe66f7a584cb3bf2d56f03fcf8378~mv2.jpg)',
-    opacity:'50%',
+    opacity: '50%',
     position: 'absolute',
-    backgroundSize:'cover',
-    backgroundRepeat:'no-repeat',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
     top: 0,
     right: 0,
     bottom: 0,
@@ -27,16 +31,16 @@ const ContentSectionOne = () => {
     return (
         <>
             <StyledBackground />
-                <StyledSection >
-                    <h1 style={{ fontSize: '62px', }}>NOSSOS SERVIÇOS DE ENGENHARIA</h1>
-                    <ul>
-                        <li>
-                            <Link to="/users">Usuários</Link>
-                        </li>
-                    </ul>
+            <StyledSection >
+                <h1 style={{ fontSize: '62px', }}>NOSSOS SERVIÇOS DE ENGENHARIA</h1>
+                <ul>
+                    <li>
+                        <Link to="/users">Usuários</Link>
+                    </li>
+                </ul>
 
-                </StyledSection>
-            
+            </StyledSection>
+
 
         </>
     )
--- src/partials/sections/Section-Two.js
+++ src/partials/sections/Section-Two.js
@@ -1,9 +1,22 @@
 import { styled } from "@mui/material"
 
 
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+
+
+
+
 const StyledSection = styled('section')(() => ({
     background: 'withe',
-    height: '100vh'
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'space-around'
 }))
 
 
@@ -14,15 +27,76 @@ const ContentSectionTwo = () => {
     return (
         <>
             <StyledSection >
-                <section >
-                    <div>
-                        <h1>
-                            BLOCO
-                        </h1>
-                    </div>
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                        sx={{ height: 140 }}
+                        image="https://static.wixstatic.com/media/b65cfd_8865fcfce5364f11831ccc7f0eab3e81~mv2.jpg/v1/fill/w_300,h_227,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/pmoc.jpg"
+                        title="PMOC"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            PMOC
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            A Treon Engenharia é referência no setor de refrigeração e climatização, onde nossos engenheiros possuem expertise na elaboração do plano de manutenção, operação e controle (PMOC). Também realizamos a manutenção preventiva e corretiva de ar condicionados de todos modelos.
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">COMPARTILHAR</Button>
+                        <Button size="small">SABER MAIS</Button>
+                    </CardActions>
+                </Card>
 
-                </section>
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                        sx={{ height: 140 }}
+                        image="https://static.wixstatic.com/media/b65cfd_8bb602e949a0413284ef77c7be5fcbfe~mv2.jpg/v1/fill/w_300,h_227,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/ppci.jpg"
+                        title="PMOC"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            PROJETOS DE PREVENÇÃO A INCÊNDIO
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Contamos com um time de projetistas experientes para regularizar sua empresa junto ao corpo de bombeiro. Elaboramos o projeto de prevenção e combate a incêndio (PPCI) conforme sua necessidade. Também te ajudamos na liberação do alvará de funcionamento.
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">COMPARTILHAR</Button>
+                        <Button size="small">SABER MAIS</Button>
+                    </CardActions>
+                </Card>
 
+                <Card sx={{ maxWidth: 345 }}>
+                    <CardMedia
+                        sx={{ height: 140 }}
+                        image="https://static.wixstatic.com/media/b65cfd_d24eb9faae4c4960a229e8fc6baebdc0~mv2.jpg/v1/fill/w_300,h_227,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/alcb_edited.jpg"
+                        title="PMOC"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            ALVARÁ DE FUNCIONAMENTO CBMES
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Ajudamos empresas de todos os segmentos na regularização e liberação dos alvarás de funcionamento junto ao CBMES.
+                            <ul style={{paddingLeft:'20px', paddingTop:'30px'}}>
+                                <li>
+                                    Consultoria de ALCB;
+                                </li>
+                                <li>
+                                    Renovação de ALCB;
+                                </li>
+                                <li>
+                                    Realização de testes e emissão de laudos técnicos.
+                                </li>
+                            </ul>
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small">COMPARTILHAR</Button>
+                        <Button size="small">SABER MAIS</Button>
+                    </CardActions>
+                </Card>
             </StyledSection>
 
         </>
