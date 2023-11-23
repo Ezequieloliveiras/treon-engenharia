a4f65a2a519d1c8d9da2a6f241f9be56682618ef
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2023-11-22 22:17:37 -0300

    adicionando painel de texto e imagem na pagina sobre

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,10 +1,19 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import styled from "@emotion/styled"
+
+const StyledSection = styled('section')(() => ({
+    position: 'relative',
+    backgroundColor: '#093170',
+    height: '100vh',
+
+}))
+
 
 function Users() {
     return (
-        <>
-            <h1>
+        <StyledSection >
+            <h1 style={{ color: 'white' }}>
                 Sobre
             </h1>
             <ul>
@@ -12,8 +21,19 @@ function Users() {
                     <Link to="/">Home</Link>
                 </li>
             </ul>
-        </>
+            <div style={{ height: '350px', width: '600px', border: '1px solid grey', borderRadius: '10px', backgroundColor: 'white', padding: '15px', position: 'absolute', top: '200px', right: '200px'}}>
+                <p style={{ color: 'grey' }}>
+                    imagem
+                </p>
+            </div>
+            <div style={{ height: '300px', width: '500px', border: '1px solid grey', borderRadius: '10px', backgroundColor: 'white', padding: '15px', position: 'absolute', top: '70px', right: '700px' }}>
+                <p style={{ color: 'grey' }}>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                </p>
+            </div>
+        </StyledSection>
     )
 }
 
 export default Users
+
