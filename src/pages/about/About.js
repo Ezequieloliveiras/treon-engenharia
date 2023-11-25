cb3df51da4edfa9c926c2ad9170aed20eb55749d
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2023-11-25 15:47:49 -0300

    corrigindo caminhos dos arquivos

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,12 +1,12 @@
 import React from "react"
 
-import Background from "../about/imageBackground/ImageBackground"
+import Background from "./components/imageBackground/ImageBackground"
 import Container from "./components/containers/Container"
-import Content from "./components/content/Content"
-import PrimaryBox from "./aboutStyles/boxes/PrimaryBox"
-import SecondaryBox from "./aboutStyles/boxes/SecondaryBox"
-import Text from "./components/text/Text"
-import ImageSobre from "./components/imageAbout/ImageAbout"
+import Content from "./components/containers/content/Content"
+import PrimaryBox from "./components/containers/content/boxes/PrimaryBox"
+import SecondaryBox from "./components/containers/content/boxes/SecondaryBox"
+import Text from "./components/containers/content/boxes/text/Text"
+import ImageSobre from "./components/containers/content/boxes/imageAbout/ImageAbout"
 
 function Sobre() {
     return (
