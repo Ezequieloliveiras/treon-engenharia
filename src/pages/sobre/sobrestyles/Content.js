e9a19fe3e00a47f0bb25f6c2a21ef1de4700e2da
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2023-11-25 13:12:37 -0300

    criando componetes

---
+++ src/pages/sobre/sobrestyles/Content.js
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled"
+
+const Content = styled('div')(() => ({
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    position:'absolute',
+    top:'-70px'
+}))
+
+export default Content
---
+++ src/pages/sobre/sobrestyles/boxes/SecondaryBox.js
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled"
+
+const SecondaryBox = styled('div')(() => ({
+    height: '250px',
+    width: 'auto'
+}))
+
+export default SecondaryBox
