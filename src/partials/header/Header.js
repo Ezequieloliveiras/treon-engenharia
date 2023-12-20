c10f88d8a6c24767bee2f709cd7811adf5e64b0a
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2023-12-20 19:45:51 -0300

    ajustando texto do menu

--- src/partials/header/Header.js
+++ src/partials/header/Header.js
@@ -6,13 +6,13 @@ import CreaLogo from '../logo/crea_logo.png';
 
 function BasicExample() {
   return (
-    <Navbar expand="lg" variant="dark" style={{ backgroundColor: '#093170' }}>
-      <Container id='contend' className="d-flex justify-content-center align-items-center">
+    <Navbar expand="lg" variant="dark" style={{ backgroundColor: '#093170', position:'relative' }}>
+      <Container id='contend' className="d-flex justify-content-center align-items-center" >
         <img src={LogoTreon} style={{ width: '10%', marginRight: '50px' }} alt="LogoTreon" />
         <img src={CreaLogo} style={{ width: '3%', marginRight: '50px' }} alt="CreaLogo" />
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+        <Navbar.Collapse id="basic-navbar-nav" style={{ alignContent:'center', justifyContent:'center'}}>
+          <Nav className="me-auto" style={{margin:'0 auto'}}>
             <Nav.Link href="/" id="custom-nav-link">Inicio</Nav.Link>
             <Nav.Link href="/contact" id="custom-nav-link">Contato</Nav.Link>
             <Nav.Link href="/sobre" id="custom-nav-link">Sobre</Nav.Link>
