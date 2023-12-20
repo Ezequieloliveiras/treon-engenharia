edd381a504d86b419fda135e7cac63d034afbdbb
ezequiel oliveira <ezequieloliveiraes@outlook.com>
2023-12-20 12:31:39 -0300

    adicionando link de whatsapp no botao

--- src/partials/buttonDialogo/Button.js
+++ src/partials/buttonDialogo/Button.js
@@ -1,6 +1,14 @@
 import './index.css'
 
 function ButtonDialogo() {
+    const handleWhatsAppClick = () => {
+        const phoneNumber = '+5527996213588'
+        const message = encodeURIComponent('Olá, estou entrando em contato pelo site.')
+
+        // Gere o link do WhatsApp com o número de telefone e mensagem pré-definida
+        const whatsappURL = `https://wa.me/${phoneNumber}?text=${message}`
+        window.open(whatsappURL)
+    }
     return (
         <>
             <a href="#" class="animated-button1"
@@ -9,7 +17,10 @@ function ButtonDialogo() {
                     position: 'absolute',
                     left: '50%',
                     transform: 'translate(-50%, -50%)'
-                }}>
+                }}
+
+                onClick={handleWhatsAppClick}
+            >
                 <span></span>
                 <span></span>
                 <span></span>
